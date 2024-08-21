1529de147aaecf7b4fc9ff639b140b2e3d46d39d
helloleesul <suerish.e@gmail.com>
2024-08-21 13:41:26 +0900

    :memo: 환경 변수 객체 파일

---
+++ src/constants/environment.ts
@@ -0,0 +1,3 @@
+export const WEATHER_API_KEY = import.meta.env.VITE_APP_WEATHER_API_KEY;
+export const GOOGLE_MAP_API_KEY = import.meta.env.VITE_APP_GOOGLE_MAP_API_KEY;
+export const GOOGLE_MAP_ID = import.meta.env.VITE_APP_GOOGLE_MAP_ID;
