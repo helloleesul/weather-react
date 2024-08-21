1f1bff01d6e8c556954293ce5518e227b50cd827
helloleesul <suerish.e@gmail.com>
2024-08-21 13:45:43 +0900

    :sparkles: 날씨 페이지 컴포넌트

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -1,7 +1,35 @@
+import WeatherNow from "@/pages/Weather/components/WeatherNow.tsx";
+import HourTemperature from "@/pages/Weather/components/HourTemperature.tsx";
+import DayTemperature from "@/pages/Weather/components/DayTemperature.tsx";
+import WeatherDetails from "@/pages/Weather/components/WeatherDetails.tsx";
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import WeatherMap from "@/pages/Weather/components/WeatherMap.tsx";
+import WeatherMapMarker from "@/pages/Weather/components/WeatherMapMarker.tsx";
+import { GOOGLE_MAP_API_KEY } from "@/constants/environment.ts";
+
+const render = (status: Status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>로딩 중...</>;
+    case Status.FAILURE:
+      return <>에러 발생</>;
+    case Status.SUCCESS:
+      return (
+        <WeatherMap>
+          <WeatherMapMarker />
+        </WeatherMap>
+      );
+  }
+};
+
 const Weather = () => {
   return (
-    <div className="h-[100rem]">
-      <section>Weather</section>
+    <div className="flex flex-col gap-4">
+      <WeatherNow />
+      <HourTemperature />
+      <DayTemperature />
+      <Wrapper apiKey={GOOGLE_MAP_API_KEY} render={render} />
+      <WeatherDetails />
     </div>
   );
 };
