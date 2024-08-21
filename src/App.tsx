cda8b7fbdc7d4a8df6e4ed287d85d8cf53da994b
helloleesul <suerish.e@gmail.com>
2024-08-21 13:33:16 +0900

    :sparkles: 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "@/components/Header";
+import { ROUTES } from "@/routes/routes.ts";
+
+const backgroundColors = {
+  [ROUTES.WEATHER]: "bg-[#6a92c4]",
+  [ROUTES.CALCULATOR]: "bg-black",
+};
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <>
-      <Outlet />
-    </>
+    <div className={`app-container ${backgroundColors[pathname]}`}>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Nav from "@/components/Nav";
+
+const Header = () => {
+  return (
+    <header className="sticky top-4 z-10">
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
