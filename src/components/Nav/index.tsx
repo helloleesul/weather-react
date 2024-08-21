0d3ad9e396a18b12171f49dc798a12f37591daca
helloleesul <suerish.e@gmail.com>
2024-08-21 13:30:53 +0900

    :sparkles: 네비게이션 추가

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,25 @@
+import { NAVIGATION } from "@/constants/navigation.ts";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="flex gap-3">
+        {NAVIGATION.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link
+                to={item.path}
+                className="inline-block text-white bg-opacity-30 bg-white py-2 px-4 rounded-lg backdrop-blur font-medium"
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/constants/navigation.ts
@@ -0,0 +1,12 @@
+import { ROUTES } from "@/routes/routes.ts";
+
+export const NAVIGATION = [
+  {
+    title: "날씨",
+    path: ROUTES.WEATHER,
+  },
+  {
+    title: "계산기",
+    path: ROUTES.CALCULATOR,
+  },
+];
