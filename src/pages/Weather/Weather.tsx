c7417f43a8997f25a78075db97b4f3990b5470a7
helloleesul <suerish.e@gmail.com>
2024-08-22 11:19:29 +0900

    :speech_balloon: 날씨 맵 상태 텍스트

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -10,9 +10,9 @@ import { GOOGLE_MAP_API_KEY } from "@/constants/environment.ts";
 const render = (status: Status) => {
   switch (status) {
     case Status.LOADING:
-      return <>로딩 중...</>;
+      return <div>Loading...</div>;
     case Status.FAILURE:
-      return <>에러 발생</>;
+      return <div>Error!</div>;
     case Status.SUCCESS:
       return (
         <WeatherMap>
