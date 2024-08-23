1f78109bc92f163bffe3b5a940450a0d8101b162
helloleesul <suerish.e@gmail.com>
2024-08-24 03:49:17 +0900

    :art: Suspense fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "@/styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "@/routes";
+import LoadingPage from "@/pages/LoadingPage.tsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Suspense fallback={<LoadingPage />}>
+      <RouterProvider router={router} />
+    </Suspense>
   </StrictMode>,
 );
