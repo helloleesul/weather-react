cf006fa2d1b006a6d31381a3a9428d89d2d74d24
helloleesul <suerish.e@gmail.com>
2024-08-24 03:44:36 +0900

    :art: 웹워커 코드정리

--- src/utils/weatherWorker.ts
+++ src/utils/weatherWorker.ts
@@ -1,30 +1,44 @@
 import { fetchWeatherData } from "@/api/weatherApi.ts";
+import { CITIES } from "@/constants/cities.ts";
 
-// let regionIndex = 0;
+let intervalId: NodeJS.Timeout | null = null;
 
 self.onmessage = async (event: MessageEvent) => {
-  const cities = event.data;
+  // 실행되는 인터벌을 중지하고 워커 종료
+  if (event.data === "STOP") {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+    }
+    self.close();
+    return;
+  }
+
+  // 도시 인덱스
+  let { cityIndex } = event.data;
 
   try {
-    const initData = await fetchWeatherData(cities[0]);
+    // 초기 날씨 데이터 fetch 및 전달
+    const initData = await fetchWeatherData(CITIES[cityIndex]);
     self.postMessage({
       weather: initData,
+      cityIndex,
     });
 
-    // setInterval(async () => {
-    //   regionIndex = (regionIndex + 1) % cities.length;
-    //
-    //   try {
-    //     const weatherData = await fetchWeatherData(cities[regionIndex]);
-    //     self.postMessage({
-    //       weather: weatherData,
-    //     });
-    //   } catch (error) {
-    //     console.error("주기적인 데이터 업데이트 중 오류 발생:", error);
-    //   }
-    // }, 10000);
+    // 10초마다 도시 인덱스를 업데이트하고 날씨 데이터 fetch
+    intervalId = setInterval(async () => {
+      cityIndex = (cityIndex + 1) % CITIES.length;
+
+      try {
+        const weatherData = await fetchWeatherData(CITIES[cityIndex]);
+        self.postMessage({
+          weather: weatherData,
+          cityIndex,
+        });
+      } catch (error) {
+        console.error("데이터 업데이트 중 오류 발생:", error);
+      }
+    }, 10000);
   } catch (error) {
-    // 초기 데이터 페칭 중 오류 처리
     console.error("초기 데이터 페칭 중 오류 발생:", error);
   }
 };
