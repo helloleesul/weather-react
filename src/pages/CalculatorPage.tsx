21f63625ace44a3c752566378c9c345992567a5c
helloleesul <suerish.e@gmail.com>
2024-08-26 01:29:42 +0900

    :sparkles: 계산기 페이지

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,40 +1,23 @@
-import useCalculator from "@/hooks/useCalculator.ts";
-import { CALCULATOR_BUTTONS } from "@/constants/elements.ts";
-
-const buttonStyle = {
-  number: "text-[40px] bg-[#333333] text-white",
-  operator: "text-[46px] bg-[#ff9f0b] pb-1",
-  setting: "text-3xl bg-[#a5a5a5] text-[#111111] font-medium",
-};
+import { useState } from "react";
+import History from "@/components/Calculator/History.tsx";
+import Toast from "@/components/Calculator/Toast.tsx";
+import Calculators from "@/components/Calculator/Calculators.tsx";
+import { History as HistoryIcon } from "lucide-react";
 
 const CalculatorPage = () => {
-  const { input, handleInput, onCalculator, onAllClean } = useCalculator();
-
-  const handleClick: { [key: string]: () => void } = {
-    calculator: onCalculator,
-    allClean: onAllClean,
-  };
+  const [showHistory, setShowHistory] = useState(false);
 
   return (
     <div>
-      <input
-        className="w-full cursor-default border-none bg-transparent text-white text-right text-6xl mb-2.5"
-        type="text"
-        value={input}
-        readOnly
-      />
-      <section className="grid grid-cols-4 gap-2.5">
-        {CALCULATOR_BUTTONS.map((item, index) => (
-          <button
-            key={index}
-            value={item.value}
-            className={`border-none h-[96px] rounded-full cursor-pointer ${buttonStyle[item.className]}`}
-            onClick={handleClick[item.value] || handleInput}
-          >
-            {item.text || item.value}
-          </button>
-        ))}
-      </section>
+      {showHistory && <History onClose={() => setShowHistory(false)} />}
+      <Toast />
+      <Calculators />
+      <button
+        onClick={() => setShowHistory((prev) => !prev)}
+        className="border-none cursor-pointer absolute right-8 bottom-5"
+      >
+        <HistoryIcon color="#ff9f0b" size={50} strokeWidth={1} />
+      </button>
     </div>
   );
 };
